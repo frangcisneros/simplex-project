88d7c95fff1cca0e26e882059f59141f1db824b2
Francisco Cisneros <franbichir@gmail.com>
2025-10-21 13:27:34 -0300

    multiple fixes

--- src/solver.py
+++ src/solver.py
@@ -5,7 +5,7 @@ Contiene la lógica de iteración y control del método simplex.
 
 from typing import Dict, Any, List
 import numpy as np
-from tableau import Tableau
+from .tableau import Tableau
 
 
 class SimplexSolver:
